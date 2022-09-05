1845e717c573c50ecf63d6ba73cf89feb711583f
Emily Sharp <emmysharp@gmail.com>
2022-09-05 10:54:27 -0400

    Frontend updates
    Implemented UI for line count and code editor EditText

--- app/src/main/java/com/esharps/emcoded/MainActivity.java
+++ app/src/main/java/com/esharps/emcoded/MainActivity.java
@@ -6,7 +6,9 @@ import java.util.*;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -36,12 +38,14 @@ import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText codeEditTxt;
-    private Button submitCode;
-    private TextView codeEditorView;
+    // UI Elements
+    private TextView lineNumberRows;
+    private EditText codeEditorText;
+    private Button submitBtn;
+
 
     RequestQueue queue;
-    String replitUrl = "https://eval-backend.emmy-sharp.repl.co/exec";
+    String url = "https://eval-backend.emmy-sharp.repl.co/exec";
     String command = "x = 'Hello World!'\nprint(x)";
     String language = "python";
     JSONObject tempResponse;
@@ -51,44 +55,68 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        lineNumberRows = findViewById(R.id.lineNumberRows);
+        codeEditorText = findViewById(R.id.codeEditorText);
 
-
-
-
-
-        queue = Volley.newRequestQueue(this);
-
-        JSONObject jsonParams = new JSONObject();
-        try {
-            jsonParams.put("language", language.toString());
-            jsonParams.put("command", command.toString());
-
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.PUT,
-                replitUrl, jsonParams, new Response.Listener<JSONObject>() {
+        codeEditorText.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onResponse(JSONObject response) {
-                tempResponse = response;
-                //Log.d("Replit Response", tempResponse.toString());
-                try {
-                    Log.d("Replit Response", tempResponse.toString());
-                } catch (Exception e) {
-                    Log.d("API callback error", e.getMessage());
-                }
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
             }
-        },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.d("Error response", error.toString());
-                    }
-                }
-        );
 
-        queue.add(jsonObjectRequest);
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                int numLines = codeEditorText.getLineCount();
+                String lineText = "";
+
+                for(int y = 1; y <= numLines; y++) {
+                    lineText = lineText + y + "\n";
+                }
+                lineNumberRows.setText(lineText);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+
+//
+//        queue = Volley.newRequestQueue(this);
+//
+//        JSONObject jsonParams = new JSONObject();
+//        try {
+//            jsonParams.put("language", language.toString());
+//            jsonParams.put("command", command.toString());
+//
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
+//        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.PUT,
+//                url, jsonParams, new Response.Listener<JSONObject>() {
+//            @Override
+//            public void onResponse(JSONObject response) {
+//                tempResponse = response;
+//                //Log.d("Replit Response", tempResponse.toString());
+//                try {
+//                    Log.d("Replit Response", tempResponse.toString());
+//                } catch (Exception e) {
+//                    Log.d("API callback error", e.getMessage());
+//                }
+//
+//            }
+//        },
+//                new Response.ErrorListener() {
+//                    @Override
+//                    public void onErrorResponse(VolleyError error) {
+//                        Log.d("Error response", error.toString());
+//                    }
+//                }
+//        );
+//
+//        queue.add(jsonObjectRequest);
 
 
     } // end of onCreate()
